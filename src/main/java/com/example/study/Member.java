e043f325fe3d2c18c9cc641d89f495eb72a41f7c
shnmj <shnmje@gmail.com>
2024-10-02 14:09:07 +0900

    중간저장

--- src/main/java/com/example/study/Member.java
+++ src/main/java/com/example/study/Member.java
@@ -1,24 +1,21 @@
 package com.example.study;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
 public class Member {
     private int id;
     private String username;
     private String password;
     private String email;
 
+    @Builder
     public Member(int id, String username, String password, String email) {
-
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
     }
-
-
 }
 
